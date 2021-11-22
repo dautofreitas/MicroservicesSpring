bb3f9c5cb77e97f765c2bf1a054453d1307528ec
adauto-freitas <adauto-freitas@N1003017>
2021-11-22 14:26:58 -0300

    RestTemplate implementation

--- hr-payroll/src/main/java/com/dautofreitas/hrpayroll/entities/Worker.java
+++ hr-payroll/src/main/java/com/dautofreitas/hrpayroll/entities/Worker.java
@@ -1,17 +1,10 @@
-package com.dautofreitas.hrworker.entities;
+package com.dautofreitas.hrpayroll.entities;
 
 import java.io.Serializable;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Entity
 public class Worker  implements Serializable {	
 	private static final long serialVersionUID = 1L;
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
 	private Long id;
 	private String name;
 	private Double dailyIncome;
