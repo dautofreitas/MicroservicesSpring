a845f23884f08cd4fc98ba3fc012faf18400b863
adauto-freitas <adauto-freitas@N1003017>
2021-11-22 17:22:42 -0300

    Ribbon load balancing

--- hr-worker/src/main/java/com/dautofreitas/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/dautofreitas/hrworker/resources/WorkerResource.java
@@ -2,7 +2,10 @@ package com.dautofreitas.hrworker.resources;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,9 +16,14 @@ import com.dautofreitas.hrworker.entities.Worker;
 import com.dautofreitas.hrworker.repositories.WorkerRepository;
 
 
+
 @RestController
 @RequestMapping(value="/workers")
 public class WorkerResource {
+	
+	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+	@Autowired
+	private Environment env;
 	
 	@Autowired
 	private WorkerRepository repository;
@@ -30,6 +38,8 @@ public class WorkerResource {
 	
 	@GetMapping(value="/{id}")
 	public ResponseEntity<Worker> findAll(@PathVariable Long id){
+		
+		logger.info("PORT= "+ env.getProperty("local.server.port"));
 		
 		Worker worker = repository.findById(id).get();
 		
