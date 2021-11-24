95e79aadb256bc2bdc7a5339e218e075b5b928d6
adauto-freitas <adauto-freitas@N1003017>
2021-11-24 11:30:20 -0300

    Configuring Zuul api gateway

--- hr-api-gateway-zuul/src/main/java/com/dautofreitas/hrapigatewayzuul/HrApiGatewayZuulApplication.java
+++ hr-api-gateway-zuul/src/main/java/com/dautofreitas/hrapigatewayzuul/HrApiGatewayZuulApplication.java
@@ -1,8 +1,13 @@
 package com.dautofreitas.hrapigatewayzuul;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableEurekaClient
+@EnableZuulProxy
 @SpringBootApplication
 public class HrApiGatewayZuulApplication {
 
