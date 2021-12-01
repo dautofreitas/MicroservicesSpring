91366348510f57dba34f344b8fe3b80f39fd1b2b
adauto-freitas <adauto-freitas@N1003017>
2021-12-01 17:01:19 -0300

    hr-oauth created

---
+++ hr-oauth/src/test/java/com/dautofreitas/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dautofreitas.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
