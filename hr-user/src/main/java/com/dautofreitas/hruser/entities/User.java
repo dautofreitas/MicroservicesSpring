79dce9fe9efcd28d4986c7a86a5614c32947ceb8
adauto-freitas <adauto-freitas@N1003017>
2022-01-28 19:14:36 -0300

    hr-user postgres dev profile

--- hr-user/src/main/java/com/dautofreitas/hruser/entities/User.java
+++ hr-user/src/main/java/com/dautofreitas/hruser/entities/User.java
@@ -13,8 +13,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="tb_user")
 public class User implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
